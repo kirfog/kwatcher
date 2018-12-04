9c475c55c7c4260eee245f6fce4cc32f9fa27337
kadmin <kirfog@gmail.com>
2018-12-04 15:39:46 +0300

    3.7.1

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 import feedparser
 import requests
 import  time
-session = requests.session()
-session.proxies = {}
+
+#session = requests.session()
+#session.proxies = {}
 #session.proxies['http'] = 'socks5h://localhost:9050'
 #session.proxies['https'] = 'socks5h://localhost:9050'
+
 url = "https://api.telegram.org/bot634132320:AAG52vGYQkoJYwrMoB2AFLQmkPz7SCMyaf0/"
 
 def get_updates_json(request):
     params = {'timeout': 100, 'offset': None}
-    response = session.get(request + 'getUpdates', data=params)
+    response = request.get(request + 'getUpdates', data=params)
     return response.json()
 
 def last_update(data):
